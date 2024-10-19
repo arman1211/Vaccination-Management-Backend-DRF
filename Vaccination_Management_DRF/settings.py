7d5ab84e6634a59f34896675ee16767a26efd20f
arman1211 <sharfuddinarman512@gmail.com>
2024-10-19 16:33:25 +0600

    database changed

--- Vaccination_Management_DRF/settings.py
+++ Vaccination_Management_DRF/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-h8&z&@u8-312v0bwk*)9lq&9uuv_^ge#u!lf(%_prk@yjwp1d%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
