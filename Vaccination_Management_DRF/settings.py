3deef85131503e3a10e1fa22c83ea2a836d22593
arman1211 <sharfuddinarman512@gmail.com>
2024-10-19 16:16:00 +0600

    database changed

--- Vaccination_Management_DRF/settings.py
+++ Vaccination_Management_DRF/settings.py
@@ -117,23 +117,23 @@ WSGI_APPLICATION = 'Vaccination_Management_DRF.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres.stdyalacexzqydvdqrdr',
-#         'PASSWORD': 'Supabase15795@',
-#         'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
-#         'PORT': '6543'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.cbpyavsilciidsvghzxg',
+        'PASSWORD': 'Supabase15795@',
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+        'PORT': '6543'
+    }
+}
 
 
 # Password validation
